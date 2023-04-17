9a1d1b10ff30215256790160a417a9660f9c6122
pAoOAk14pG <bFa2xRMZ@outlook.com>
2023-04-17 15:21:42 +0800

    Removed more unnecessary files

--- src/hoc/appWithSession.jsx
+++
@@ -1,9 +0,0 @@
-import { SessionProvider } from 'next-auth/react'
-
-export default function appWithSession(Component) {
-  return (props) => (
-    <SessionProvider session={props?.pageProps?.session}>
-      <Component {...props} />
-    </SessionProvider>
-  )
-}
